8697193c8c7f565f5280bad0f17ca59a854b229c
David Agudelo <david.agudelo@smartsoft.com.co>
2022-09-29 14:31:41 -0500

    Integracion menu version mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const dropmenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHumIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-dropmenu.addEventListener('click',toggDesktopMenu);
+dropmenu.addEventListener('click', toggDesktopMenu);
+menuHumIcon.addEventListener('click', toggMobileMenu )
 
 function toggDesktopMenu(){
-    console.log("Click");
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggMobileMenu(){
+    console.log("Click");
+    mobileMenu.classList.toggle('inactive');
 }
